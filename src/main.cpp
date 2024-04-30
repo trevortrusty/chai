d772bc01acc9f3ad00fc409f888c9e4e3a3e150e
Trevor <trevortrusty@outlook.com>
2024-04-29 23:51:49 -0400

    remove commented out struct from code

--- src/main.cpp
+++ src/main.cpp
@@ -9,19 +9,6 @@
 const int TOP_SPACING = 0;
 const int LEFT_SPACING = 6;
 
-// struct Screen
-// {
-//     int maxY, maxX;         // Max cursor positions for entire terminal (stdscr)
-//     int winMaxY, winMaxX;   // Max cursor positions for just the editor window
-
-//     int curY, curX;
-//     int numberOfLines = 1;  // Number of lines that have been created/exist by the user
-//     int currentLine = 1;    // Line the cursor is on within the text editor
-//     int currentChar = 1;    // Char/space the cursor is on within the line
-//     int topLine = 1;        // Keeps track of the top visible line when scrolling
-//     int printToLine;        // Keeps track of where to stop reprinting lines, when a reprint is necessary
-// };
-
 // Text editor line numbering starts at 1, this function takes a line number and makes it compatible with zero-based indexing
 int zeroBased(int indexToChange)
 {
